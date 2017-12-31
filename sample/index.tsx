1bb1c5481644faaafab29fa4a13011fcd30ada2e
Ryan Price <ryanmichaelprice@gmail.com>
2017-12-31 00:04:58 -0500

    Add some sample inputs and a common padding on inputs

--- sample/index.tsx
+++ sample/index.tsx
@@ -2,18 +2,63 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as ReactDnd from "react-dnd";
 
-import {UIColorSelector} from 'ququmber-ui/controls/UIColorSelector';
+import UIColorSelector from 'ququmber-ui/controls/UIColorSelector';
+import UIEditableText from 'ququmber-ui/controls/UIEditableText';
+import UITag from 'ququmber-ui/controls/UITag';
+import UIMultiInput from 'ququmber-ui/controls/UIMultiInput';
+import UIToggle from 'ququmber-ui/controls/UIToggle';
+import UITextInput from 'ququmber-ui/controls/UITextInput';
 
-class RootComponent extends React.Component<{}, {}> {
+class RootComponent extends React.Component<{}, RootComponentState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      toggleValue: true
+    }
+  }
+
   render() {
+    const {toggleValue} = this.state;
+
     return <div>
       <UIColorSelector
         onColorChanged={console.log}
         value={null}
       />
+      <UIEditableText placeholder="Enter your text here" />
+      <div style={{margin: '10px 0px'}}>
+        <UITag name="Shopping" canRemove={true} color="4286f4" />
+        <UITag name="Work" canRemove={true} color="f4e242" />
+        <UITag name="Sideproject" canRemove={true} color="b72924" />
+        <UITag name="Untriaged" canRemove={true} color="238444" />
+      </div>
+      <UIMultiInput
+        options={[
+          {value: "1", name: "One"},
+          {value: "2", name: "Two"},
+          {value: "3", name: "Three"},
+          {value: "3", name: "Four"}
+        ]}
+        selected={["1", "2"]}
+        onOptionAdded={() => {}}
+        onOptionRemoved={() => {}}
+      />
+      <UIToggle
+        checked={toggleValue}
+        onChange={value => this.setState({toggleValue: value})}
+      />
+      <UITextInput
+        placeholder="Your name here"
+      />
     </div>;
   }
 }
+
+interface RootComponentState {
+  toggleValue: boolean;
+}
+
+
 console.log(UIColorSelector);
 const el = document.createElement('div');
 document.querySelector('body').appendChild(el);
