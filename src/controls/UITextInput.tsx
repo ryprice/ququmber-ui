b5feab772324b88587371bf6d594b3166dd281dc
Ryan Price <ryanmichaelprice@gmail.com>
2018-01-06 01:41:20 -0500

    UITextInput tabbing

--- src/controls/UITextInput.tsx
+++ src/controls/UITextInput.tsx
@@ -2,51 +2,53 @@ import * as React from "react";
 
 export class UITextInput extends React.Component<UITextInputProps, {}> {
 
-    inputEl: HTMLInputElement;
+  inputEl: HTMLInputElement;
 
-    public static defaultProps = {
-        className: "",
-        placeholder: "",
-        value: "",
-        disabled: false
-    };
+  public static defaultProps = {
+    className: "",
+    placeholder: "",
+    value: "",
+    disabled: false
+  };
 
-    onKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
-        switch (event.charCode) {
-            case 13: // enter
-                this.onSubmit();
-                break;
-            default:
-                break;
-        }
-        return true;
+  onKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+    switch (event.charCode) {
+      case 13: // enter
+        this.onSubmit();
+        break;
+      default:
+        break;
     }
+    return true;
+  }
 
-    onSubmit() {
-        const {onSubmit} = this.props;
-        onSubmit && onSubmit(this.inputEl.value);
-    }
+  onSubmit() {
+    const {onSubmit} = this.props;
+    onSubmit && onSubmit(this.inputEl.value);
+  }
 
-    render() {
-        return <input
-            className={`uiTextInput ${this.props.className}`}
-            onKeyPress={(e) => this.onKeyPress(e)}
-            ref={(el) => this.inputEl = el}
-            defaultValue={this.props.value}
-            placeholder={this.props.placeholder}
-            onBlur={() => this.onSubmit()}
-            disabled={this.props.disabled}
-        />;
-    }
+  render() {
+    return <input
+      className={`uiTextInput ${this.props.className}`}
+      onKeyPress={(e) => this.onKeyPress(e)}
+      ref={(el) => this.inputEl = el}
+      defaultValue={this.props.value}
+      placeholder={this.props.placeholder}
+      onBlur={() => this.onSubmit()}
+      disabled={this.props.disabled}
+      onChange={this.props.onChange}
+    />;
+  }
 }
 
 export interface UITextInputProps extends React.Props<HTMLInputElement> {
-    value?: string;
-    onSubmit?: (value: string) => void;
-    className?: string;
-    placeholder?: string;
-    defaultValue?: string;
-    disabled?: boolean;
+  value?: string;
+  onSubmit?: (value: string) => void;
+  className?: string;
+  placeholder?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => boolean;
 }
 
 export default UITextInput;
