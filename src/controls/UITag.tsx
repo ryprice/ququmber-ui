89125d9be0070f5dfb0db3adb389b5abcc2f40ab
Ryan Price <ryanmichaelprice@gmail.com>
2018-01-21 21:41:13 -0500

    Add border radius to UITag and format code

--- src/controls/UITag.tsx
+++ src/controls/UITag.tsx
@@ -4,24 +4,32 @@ import {isDarkColor} from "ququmber-ui/utils/colorUtils";
 
 export class UITag extends React.Component<UITagProps, {}> {
     render() {
-        const {color, name, onRemoved, canRemove} = this.props;
-        const style = color ? {background: `#${color}`} : {};
-        return <div className={`uiTag ${color && isDarkColor(color) ? 'darkText' : 'lightText'}`} style={style}>
-            {name}
-            {canRemove !== false &&
-                <button className="remove" onClick={onRemoved ? onRemoved : () => {}}>
-                    <span className="octicon octicon-x" />
-                </button>
-            }
-        </div>;
+      const {color, name, onRemoved, canRemove} = this.props;
+      const style = color ? {background: `#${color}`} : {};
+
+      const removeButton = (
+        <button
+          className="remove"
+          onClick={onRemoved ? onRemoved : () => {}}
+        >
+          <span className="octicon octicon-x" />
+        </button>
+      );
+
+      return <div
+        className={`uiTag ${color && isDarkColor(color) ? 'darkText' : 'lightText'}`}
+        style={style}>
+        {name}
+        {canRemove !== false && removeButton}
+      </div>;
     }
 }
 
 export interface UITagProps extends React.Props<UITag> {
-    name: string;
-    onRemoved?: () => void;
-    color?: string;
-    canRemove?: boolean;
+  name: string;
+  onRemoved?: () => void;
+  color?: string;
+  canRemove?: boolean;
 }
 
 export default UITag;
