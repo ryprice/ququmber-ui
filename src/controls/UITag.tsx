b6977164127272429b12a55bbb698c96c7e566f0
Ryan Price <ryanmichaelprice@gmail.com>
2019-07-26 00:20:56 -0400

    Add onClick to UITag and convert to functional

--- src/controls/UITag.tsx
+++ src/controls/UITag.tsx
@@ -1,38 +1,46 @@
 import * as React from 'react';
+import {findDOMNode} from 'react-dom';
 
 import {isDarkColor} from 'ququmber-ui/utils/colorUtils';
 
-export class UITag extends React.Component<UITagProps, {}> {
-  render() {
-    const {color, name, onRemoved, canRemove, onClick} = this.props;
-    const style = color ? {background: `#${color}`} : {};
+const {useRef} = React;
 
-    const removeButton = (
-      <button
-        className="remove"
-        onClick={onRemoved ? onRemoved : () => {}}
-      >
-        <span className="octicon octicon-x" />
-      </button>
-    );
+const UITag = (props: UITagProps) => {
+  const {color, name, onRemoved, canRemove} = props;
+  const removeButtonRef = useRef();
+  const style = color ? {background: `#${color}`} : {};
 
-    const className = (
-      'UITag ' +
-      (!color || isDarkColor(color) ? 'lightText ' : 'darkText ') +
-      (canRemove ? 'canRemove ' : '')
-    );
+  const removeButton = (
+    <button
+      ref={removeButtonRef}
+      className="remove"
+      onClick={onRemoved ? onRemoved : () => {}}>
+      <span className="octicon octicon-x" />
+    </button>
+  );
 
-    return <div
-      onClick={onClick}
-      className={className}
-      style={style}>
-      {name}
-      {canRemove !== false && removeButton}
-    </div>;
-  }
-}
+  const onClick = (e: React.MouseEvent) => {
+    if (!findDOMNode(removeButtonRef.current).contains(e.target as Node)) {
+      props.onClick();
+    }
+  };
 
-export interface UITagProps extends React.Props<UITag> {
+  const className = (
+    'UITag ' +
+    (!color || isDarkColor(color) ? 'lightText ' : 'darkText ') +
+    (canRemove ? 'canRemove ' : '')
+  );
+
+  return <div
+    onClick={onClick}
+    className={className}
+    style={style}>
+    {name}
+    {canRemove !== false && removeButton}
+  </div>;
+};
+
+export interface UITagProps {
   name: string | JSX.Element;
   onRemoved?: () => void;
   color?: string;
