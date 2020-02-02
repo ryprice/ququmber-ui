fed1129952a63d947289afe2db22dc0ce3c73475
Ryan Price <ryanmichaelprice@gmail.com>
2020-02-02 17:35:21 -0500

    lightenDarkenColor

--- src/utils/colorUtils.ts
+++ src/utils/colorUtils.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:no-bitwise */
+
 export const isDarkColor = (bgColor: string) => {
   const nThreshold = 130;
   const components = getRGBComponents(bgColor);
@@ -11,3 +13,27 @@ const getRGBComponents = (color: string) => ({
   G: parseInt(color.substring(2, 4), 16),
   B: parseInt(color.substring(4, 6), 16)
 });
+
+export const lightenDarkenColor = (hex: string, amt: number) => {
+  let usePound = false;
+  let hexWithoutPound = hex;
+  if (hex[0] === '#') {
+    hexWithoutPound = hex.slice(1);
+    usePound = true;
+  }
+  const components = getRGBComponents(hexWithoutPound);
+
+  let r = components.R + amt;
+  if (r > 255) { r = 255; }
+  else if  (r < 0) { r = 0; }
+
+  let g = components.G + amt;
+  if (g > 255) { g = 255; }
+  else if (g < 0) { g = 0; }
+
+  let b = components.B + amt;
+  if (b > 255) { b = 255; }
+  else if  (b < 0) { b = 0; }
+
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
+};
