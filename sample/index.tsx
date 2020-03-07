73feb116647dcffc4e0715f99b5122a0031efdf9
Ryan Price <ryanmichaelprice@gmail.com>
2020-03-07 13:49:46 +0000

    Filtering on dev.listlab.io/ui/ component library

--- sample/index.tsx
+++ sample/index.tsx
@@ -37,120 +37,130 @@ import UIMessageModalSample from './popup/UIMessageModalSample';
 import UIPopupSample from './popup/UIPopupSample';
 import UITooltipSample from './popup/UITooltipSample';
 
-class RootComponent extends React.Component<{}, RootComponentState> {
+const {useCallback, useEffect, useState} = React;
 
-  readonly state: RootComponentState = {
-    curNav: null
+const RootComponent = () => {
+  const [curNav, setCurNav] = useState<string>();
+
+  const onHashChange = () => {
+    const path = window.location.hash.substr(1, window.location.hash.length);
+    setCurNav(path);
   };
 
-  componentDidMount() {
-    window.addEventListener(
-      'hashchange',
-      () => this.onHashChange(),
-      false
-    );
+  useEffect(() => {
+    window.addEventListener('hashchange', onHashChange, false);
+    onHashChange();
+  }, []);
 
-    this.onHashChange();
-  }
+  
+  const sections = [
+    'Controls',
+    ['ququmber-ui/controls/UIColorSelector', <UIColorSelectorSample />],
+    ['ququmber-ui/controls/UITag', <UITagSample />],
+    ['ququmber-ui/controls/UIToggle', <UIToggleSample />],
+    ['ququmber-ui/controls/UICheckbox', <UICheckbox />],
+    ['ququmber-ui/controls/UIToastNotification', <UIToastNotificationSample />],
+    ['ququmber-ui/controls/UIBadge', <div>
+      <UIBadge color={Colors.GO} text="Activated" />
+      &nbsp;&nbsp;
+      <UIBadge color={Colors.DISABLED} text="Disabled" />
+    </div>],
 
-  onHashChange() {
-    const path = window.location.hash.substr(1, window.location.hash.length);
-    this.setState({curNav: path});
-  }
+    'Input',
+    ['ququmber-ui/input/UICopyInput', <UICopyInput
+      value="http://app.listlab.io/t/1839483?s=7d92kd0mc283mns0yu44j"
+    />],
+    ['ququmber-ui/input/UIEditableText', <UIEditableText
+      placeholder="Enter your text here"
+    />],
+    ['ququmber-ui/input/UIMultiInput', <UIMultiInputSample />],
+    ['ququmber-ui/input/UITextInput', <UITextInput
+      placeholder="Your name here"
+    />],
+    ['ququmber-ui/input/UISelect', <UISelectSample />],
 
-  render() {
-    const sections = [
-      'Controls',
-      ['ququmber-ui/controls/UIColorSelector', <UIColorSelectorSample />],
-      ['ququmber-ui/controls/UITag', <UITagSample />],
-      ['ququmber-ui/controls/UIToggle', <UIToggleSample />],
-      ['ququmber-ui/controls/UICheckbox', <UICheckbox />],
-      ['ququmber-ui/controls/UIToastNotification', <UIToastNotificationSample />],
-      ['ququmber-ui/controls/UIBadge', <div>
-        <UIBadge color={Colors.GO} text="Activated" />
-        &nbsp;&nbsp;
-        <UIBadge color={Colors.DISABLED} text="Disabled" />
-      </div>],
+    'Popup',
+    ['ququmber-ui/popup/UIMessageModal', <UIMessageModalSample />],
+    ['ququmber-ui/popup/UITooltip', <UITooltipSample />],
+    ['ququmber-ui/popup/UIPopup', <UIPopupSample />],
 
-      'Input',
-      ['ququmber-ui/input/UICopyInput', <UICopyInput
-        value="http://app.listlab.io/t/1839483?s=7d92kd0mc283mns0yu44j"
-      />],
-      ['ququmber-ui/input/UIEditableText', <UIEditableText
-        placeholder="Enter your text here"
-      />],
-      ['ququmber-ui/input/UIMultiInput', <UIMultiInputSample />],
-      ['ququmber-ui/input/UITextInput', <UITextInput
-        placeholder="Your name here"
-      />],
-      ['ququmber-ui/input/UISelect', <UISelectSample />],
+    'Button',
+    ['ququmber-ui/button/UIButton', <UIButtonSample />],
+    ['ququmber-ui/button/UIDropdownButton', <UIDropdownButtonSample />],
+    ['ququmber-ui/button/UIIconButton', <div>
+      <UIIconButton icon="fa fa-inbox" /><br />
+      <div style={{fontSize: '20px'}}><UIIconButton icon="fa fa-inbox" /></div><br />
+    </div>],
 
-      'Popup',
-      ['ququmber-ui/popup/UIMessageModal', <UIMessageModalSample />],
-      ['ququmber-ui/popup/UITooltip', <UITooltipSample />],
-      ['ququmber-ui/popup/UIPopup', <UIPopupSample />],
+    'FuzzyTime',
+    ['ququmber-ui/fuzzyTime/FuzzyTimeButton', <FuzzyTimeButton />],
+    ['ququmber-ui/fuzzyTime/FuzzyTimeSelect', <FuzzyTimeSelectSample />],
 
-      'Button',
-      ['ququmber-ui/button/UIButton', <UIButtonSample />],
-      ['ququmber-ui/button/UIDropdownButton', <UIDropdownButtonSample />],
-      ['ququmber-ui/button/UIIconButton', <div>
-        <UIIconButton icon="fa fa-inbox" /><br />
-        <div style={{fontSize: '20px'}}><UIIconButton icon="fa fa-inbox" /></div><br />
-      </div>],
+    'Styles',
+    ['ququmber-ui/Colors', <ColorsSection />],
+    ['ququmber-ui/Typography', <TypographySection />],
+    ['Sample-form', <FormSection />],
 
-      'FuzzyTime',
-      ['ququmber-ui/fuzzyTime/FuzzyTimeButton', <FuzzyTimeButton />],
-      ['ququmber-ui/fuzzyTime/FuzzyTimeSelect', <FuzzyTimeSelectSample />],
+    'listlab-api',
+    ['ququmber-ui/tasks/TaskFilterLink', <TaskFilterLink
+      href="http://google.com"
+      onClick={() => alert('clicked list')}
+      onDropTasks={(taskIds: number[]) => alert('tasks dropped')}>
+      {'List of tasks'}
+    </TaskFilterLink>]
+  ];
 
-      'Styles',
-      ['ququmber-ui/Colors', <ColorsSection />],
-      ['ququmber-ui/Typography', <TypographySection />],
-      ['Sample-form', <FormSection />],
+  const curSection = sections.find(s => s[0] === curNav);
 
-      'listlab-api',
-      ['ququmber-ui/tasks/TaskFilterLink', <TaskFilterLink
-        href="http://google.com"
-        onClick={() => alert('clicked list')}
-        onDropTasks={(taskIds: number[]) => alert('tasks dropped')}>
-        {'List of tasks'}
-      </TaskFilterLink>]
-    ];
+  const [filter, setFilter] = useState<string>(null);
+  const onFilterKeyPress = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const nextFilter = event.target.value;
+    if (nextFilter != null && nextFilter.length > 0) {
+      setFilter(nextFilter);
+    } else {
+      setFilter(null);
+    }
+  }, [setFilter, filter]);
 
-    const curSection = sections.find(s => s[0] === this.state.curNav);
+  const filteredSections = sections.filter(s => {
+    if (filter != null && typeof s !== 'string') {
+      const pathParts = (s[0] as string).split('/');
+      return pathParts[pathParts.length - 1]
+        .toLocaleLowerCase()
+        .includes(filter.toLocaleLowerCase());
+    }
+    return true;
+  });
 
-    return <div>
-      <div className="Topbar">
-        <div id="logo" />
+  return <div>
+    <div className="Topbar">
+      <div id="logo" />
+    </div>
+    <div className="mainContent">
+      <div className="sideNav">
+        <UITextInput placeholder="filter" onChange={onFilterKeyPress} />
+        {filteredSections.map((s, idx) => {
+          if (typeof s === 'string') {
+            return <p className="header">{s}</p>;
+          } else {
+            const pathParts = (s[0] as string).split('/');
+
+            return <p><a href={`#${s[0]}`}>
+              {pathParts[pathParts.length - 1]}
+            </a></p>;
+          }
+        })}
       </div>
-      <div className="mainContent">
-        <div className="sideNav">
-          {sections.map((s, idx) => {
-            if (typeof s === 'string') {
-              return <p className="header">{s}</p>;
-            } else {
-              const pathParts = (s[0] as string).split('/');
 
-              return <p><a href={`#${s[0]}`}>
-                {pathParts[pathParts.length - 1]}
-              </a></p>;
-            }
-          })}
-        </div>
-
-        <div style={{flexGrow: 1}}>
-          {curSection ? <ComponentSection
-            name={curSection[0] as string}>
-            {curSection[1] as React.ReactChild}
-          </ComponentSection> : null}
-        </div>
+      <div style={{flexGrow: 1}}>
+        {curSection ? <ComponentSection
+          name={curSection[0] as string}>
+          {curSection[1] as React.ReactChild}
+        </ComponentSection> : null}
       </div>
-    </div>;
-  }
-}
-
-interface RootComponentState {
-  curNav: string;
-}
+    </div>
+  </div>;
+};
 
 const el = document.createElement('div');
 document.querySelector('body').appendChild(el);
