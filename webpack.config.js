9e3a51ab7b35e8e43485216664bb2f0aff307782
Ryan Price <ryanmichaelprice@gmail.com>
2020-03-26 23:22:49 -0400

    Use shared webpackConfigBuilders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+const {
+  eslintLoaderConfig,
+  tsLoaderConfig,
+  sassLoaderConfig,
+  pluginsConfig,
+} = require('../listlab-build/webpackConfigBuilders');
 
 module.exports = {
   entry: {
@@ -38,42 +41,11 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        loader: ['awesome-typescript-loader'],
-        exclude: [/node_modules/]
-      },
-      {
-        test: /\.tsx?$/,
-        loader: ['eslint-loader'],
-        include: [path.resolve(__dirname)],
-        exclude: [
-          /node_modules/,
-          path.resolve(__dirname + '/../listlab-api-js')
-        ]
-      },
-      {
-        test: /\.sass$/,
-        loader: [
-          ExtractTextPlugin.loader,
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: { includePaths: ['../ququmber-ui/lib'] }
-          }
-        ],
-      }
+      tsLoaderConfig(),
+      eslintLoaderConfig(),
+      sassLoaderConfig(),
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('css/[name].css', {
-      allChunks: true
-    }),
-    new CopyWebpackPlugin([
-      {from: 'dist/ququmber-ui.css', to: 'ququmber-ui.css'}
-    ]),
-    new StyleLintPlugin({
-      syntax: 'sass'
-    })
-  ]
+
+  plugins: pluginsConfig('prod'),
 };
