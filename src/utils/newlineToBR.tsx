1c8596570974d86a25a6e42ca18ce0b6165a95c2
Ryan Price <ryanmichaelprice@gmail.com>
2020-06-15 21:14:48 -0400

    newlineToBR util for converting multiline text to react friendly nodes

---
+++ src/utils/newlineToBR.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const newlineToBR = (text: string) =>
+  text.split('\n').map((line, key) =>
+    <span key={key}>{line}<br /></span>
+  );
+
+export default newlineToBR;
