849536f1bbb199ed65f6ca09b5f56c06e727db76
Ryan Price <ryanmichaelprice@gmail.com>
2021-01-14 21:52:33 -0500

    Convert UIButton to functional

--- src/button/UIButton.tsx
+++ src/button/UIButton.tsx
@@ -3,42 +3,29 @@ import * as React from 'react';
 import UILoading from 'ququmber-ui/progress/UILoading';
 import Stylings from 'ququmber-ui/Stylings';
 
-export class UIButton extends React.Component<UIButtonProps, {}> {
+const UIButton = React.forwardRef<HTMLButtonElement, UIButtonProps>(
+  (props: UIButtonProps, ref) => {
+    const {className, onClick, children, style, styling, disabled, id, loading} = props;
 
-  inputEl: HTMLInputElement;
-
-  public static defaultProps = {
-    className: '',
-    placeholder: '',
-    value: '',
-    disabled: false,
-    style: {},
-    styling: Stylings.CONTROL,
-    loading: false,
-  };
-
-  render() {
-    const {
-      className, onClick, children, style,
-      styling, disabled, id, loading
-    } = this.props;
-
-    return <button
-      className={`UIButton ${className} styling-${styling}`}
-      onClick={onClick}
-      style={style}
-      disabled={disabled}
-      id={id}
-    >
-      <span className="inner">{children}</span>
-      {loading && (
-        <div className="loadingContainer">
-          <UILoading />
-        </div>
-      )}
-    </button>;
+    return (
+      <button
+        ref={ref}
+        className={`UIButton ${className != null ? className : ''} styling-${styling || Stylings.CONTROL}`}
+        onClick={onClick}
+        style={style}
+        disabled={disabled}
+        id={id}
+      >
+        <span className="inner">{children}</span>
+        {loading && (
+          <div className="loadingContainer">
+            <UILoading />
+          </div>
+        )}
+      </button>
+    );
   }
-}
+);
 
 export type UIButtonProps = {
   onClick?: () => void;
