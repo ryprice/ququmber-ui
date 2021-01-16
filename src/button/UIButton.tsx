c5ba27e8b1c6124298bd4e4f21a976c48e67b6ef
Ryan Price <ryanmichaelprice@gmail.com>
2021-01-15 21:55:18 -0500

    Add ref back to UIButton

--- src/button/UIButton.tsx
+++ src/button/UIButton.tsx
@@ -3,29 +3,27 @@ import * as React from 'react';
 import UILoading from 'ququmber-ui/progress/UILoading';
 import Stylings from 'ququmber-ui/Stylings';
 
-const UIButton = React.forwardRef<HTMLButtonElement, UIButtonProps>(
-  (props: UIButtonProps, ref) => {
-    const {className, onClick, children, style, styling, disabled, id, loading} = props;
+const UIButton = React.forwardRef<HTMLButtonElement, UIButtonProps>((props, ref) => {
+  const {className, onClick, children, style, styling, disabled, id, loading} = props;
 
-    return (
-      <button
-        ref={ref}
-        className={`UIButton ${className != null ? className : ''} styling-${styling || Stylings.CONTROL}`}
-        onClick={onClick}
-        style={style}
-        disabled={disabled}
-        id={id}
-      >
-        <span className="inner">{children}</span>
-        {loading && (
-          <div className="loadingContainer">
-            <UILoading />
-          </div>
-        )}
-      </button>
-    );
-  }
-);
+  return (
+    <button
+      ref={ref}
+      className={`UIButton ${className != null ? className : ''} styling-${styling || Stylings.CONTROL}`}
+      onClick={onClick}
+      style={style}
+      disabled={disabled}
+      id={id}
+    >
+      <span className="inner">{children}</span>
+      {loading && (
+        <div className="loadingContainer">
+          <UILoading />
+        </div>
+      )}
+    </button>
+  );
+});
 
 export type UIButtonProps = {
   onClick?: () => void;
