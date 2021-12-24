b7b00a6e497e97c5a95be0e38cc9015fece2fc72
Ryan Price <ryanmichaelprice@gmail.com>
2021-12-24 00:13:55 -0500

    Non-clickable version of UIIDChip

--- src/chip/UIIDChip.tsx
+++ src/chip/UIIDChip.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 
 const UIIDChip = (props: UIIDChipProps) => {
-  const {children} = props;
-  return <div className="UIIDChip">
+  const {children, canClick} = props;
+  return <div className={`UIIDChip ${canClick === false ? '' : 'clickable'}`}>
     {children}
   </div>;
 };
 
 type UIIDChipProps = {
-  children: React.ReactNode
+  children: React.ReactNode;
+  canClick?: boolean;
 };
 
 export default UIIDChip;
