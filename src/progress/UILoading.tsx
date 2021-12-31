113d5d95b30aa202efe4186354abc2daf0bfab33
Ryan Price <ryanmichaelprice@gmail.com>
2021-12-30 22:05:34 -0500

    Add UILoading text option

--- src/progress/UILoading.tsx
+++ src/progress/UILoading.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 
+import Colors from 'ququmber-ui/Colors';
+
 export class UILoading extends React.Component<UILoadingProps, {}> {
   render() {
-    const {size} = this.props;
+    const {size, text} = this.props;
     const color = this.props.color || '#eeeeee';
 
     /* eslint-disable max-len */
-    return <div className="UILoading" style={{fontSize: size}}>
-      <div style={{marginLeft: '-.5em', marginTop: '-.5em'}}  dangerouslySetInnerHTML={{__html: `
+    const spinner = <div style={{fontSize: size}}>
+      <div dangerouslySetInnerHTML={{__html: `
         <svg width='1em' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-ring">
           <rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect>
           <circle cx="50" cy="50" r="40" stroke-dasharray="163.36281798666926 87.9645943005142" stroke="${color}" fill="none" stroke-width="20px">
@@ -17,12 +19,44 @@ export class UILoading extends React.Component<UILoadingProps, {}> {
       `}} />
     </div>;
     /* eslint-disable max-len */
+
+    if (text) {
+      const width = Math.max(size * 2.5, 110);
+      return  (
+        <div className="UILoading">
+          <div style={{
+            marginLeft: `-${width/2}px`,
+            marginTop: `-${size/2}px`,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center'
+          }}>
+            <div style={{width: `${size}px`, height: `${size}px`}}>
+              {spinner}
+            </div>
+            <div style={{width: `${width}px`, marginTop: '.6em', color: Colors.QUIET}}>
+              {text}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="UILoading">
+        <div style={{marginLeft: '-.5em', marginTop: '-.5em'}}>
+          {spinner}
+        </div>
+      </div>
+    );
   }
 }
 
 export type UILoadingProps = {
   size?: number;
   color?: string;
+  text?: string;
 };
 
 export default UILoading;
