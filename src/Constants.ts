7d95a388494bb5776dd906f1cba7280006f413e8
Ryan Price <ryanmichaelprice@gmail.com>
2023-06-21 23:00:18 -0400

    Additional constants and colors

--- src/Constants.ts
+++ src/Constants.ts
@@ -4,3 +4,8 @@ export const SHADOW_MODAL = '3px 3px 7px rgba(0, 0, 0, 0.4)';
 
 export const BORDER_RADIUS_BUTTON = '.25em';
 export const BORDER_RADIUS_INPUT = '3px';
+
+export const FONT_STACK = 'Arial, sans-serif';
+export const FONT_STACK_HEADERS = 'sans-serif';
+
+export const TRANSITION_DEFAULT = '0.3s';
