3c82f182a665777d3c06b373f1f8a899950d7967
Ryan Price <7935599+ryprice@users.noreply.github.com>
2023-07-26 21:24:26 -0400

    UIButton styling=null should use dark spinner

--- src/button/UIButton.tsx
+++ src/button/UIButton.tsx
@@ -2,11 +2,11 @@ import {useCallback, useState, MutableRefObject, forwardRef} from 'react';
 import {mergeRefs} from 'react-merge-refs';
 import TetherComponent from 'react-tether';
 
+import Colors from 'ququmber-ui/Colors';
 import UILoading from 'ququmber-ui/progress/UILoading';
 import Stylings from 'ququmber-ui/Stylings';
 import UIMountTransition from 'ququmber-ui/utils/UIMountTransition';
 import useDelayedMouseHover from 'ququmber-ui/utils/useDelayedMouseHover';
-import Colors from 'ququmber-ui/Colors';
 
 const UIButton = forwardRef<HTMLButtonElement, UIButtonProps>((props, ref) => {
   const {className, onClick, children, style, styling, disabled, id, loading, tooltip} = props;
@@ -18,7 +18,10 @@ const UIButton = forwardRef<HTMLButtonElement, UIButtonProps>((props, ref) => {
   const [onMouseOver, onMouseOut] = useDelayedMouseHover(setShowTooltipIfEnabled, 300, 0);
   const buttonWithMaybeTooltipProps = tooltip ? {onMouseOver, onMouseOut} : {};
 
-  const darkSpinner = [Stylings.CONTROL, Stylings.FLAT, Stylings.OUTLINE, Stylings.MATTE].includes(styling);
+  const darkSpinner = (
+    styling == null ||
+    [Stylings.CONTROL, Stylings.FLAT, Stylings.OUTLINE, Stylings.MATTE].includes(styling)
+  );
 
   const renderButton = (tetherRef: MutableRefObject<HTMLButtonElement>) => (
     <button
@@ -33,7 +36,7 @@ const UIButton = forwardRef<HTMLButtonElement, UIButtonProps>((props, ref) => {
       <span className="inner">{children}</span>
       {loading && (
         <div className="loadingContainer">
-          <UILoading {...(darkSpinner ? {color: Colors.BASEFONT} : {})}/>
+          <UILoading {...(darkSpinner ? {color: Colors.BASEFONT} : {})} />
         </div>
       )}
     </button>
