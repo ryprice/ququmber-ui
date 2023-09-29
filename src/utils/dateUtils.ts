38ebc85e66efb200bfc0587c044df25812a7b879
Ryan Price <7935599+ryprice@users.noreply.github.com>
2023-09-29 10:34:44 -0400

    Share date/time formatting

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -16,3 +16,38 @@ export const monthStartDay = (month: number, year: number): number => {
   firstOfMonth.setFullYear(year, month, 1);
   return firstOfMonth.getDay();
 };
+
+export const formatShortDate = (date: Date) => {
+  return date && `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+};
+
+export const formatShortDateTime = (time: Date) => (
+  `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()} ` +
+  `${time.getHours()}:${time.getMinutes()}`
+);
+
+export const formatRelativeTime = (date: Date): string => {
+  const currentDate = new Date();
+  const timeDifference = currentDate.getTime() - date.getTime();
+
+  // Define time intervals in milliseconds
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const week = 7 * day;
+
+  if (timeDifference < minute) {
+    return 'just now';
+  } else if (timeDifference < hour) {
+    const minutesAgo = Math.floor(timeDifference / minute);
+    return `${minutesAgo} minute${minutesAgo > 1 ? 's' : ''} ago`;
+  } else if (timeDifference < day) {
+    const hoursAgo = Math.floor(timeDifference / hour);
+    return `${hoursAgo} hour${hoursAgo > 1 ? 's' : ''} ago`;
+  } else if (timeDifference < week) {
+    const daysAgo = Math.floor(timeDifference / day);
+    return `${daysAgo} day${daysAgo > 1 ? 's' : ''} ago`;
+  } else {
+    return formatShortDate(date);
+  }
+};
